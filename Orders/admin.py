056ef64c391fa4f2888140ecd38a191c78499ff0
brentwilson <brent@prmconnect.com>
2024-09-22 17:48:46 -0700

    init commit

---
+++ Orders/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+from unfold.admin import TabularInline
+
+# Register your models here.
+from .models import Quote, QuoteItems
+
+class QuoteItemsInline(TabularInline):
+    model = QuoteItems
+
+@admin.register(Quote)
+class QuoteAdmin(ModelAdmin):
+    list_display = ['company', 'total_price', 'status']
+    search_fields = ['company','status']
+    list_filter = ['status', 'company']
+    inlines = [QuoteItemsInline]
+
+
+
+
+
+
---
+++ Pages/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to='images/pages')
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
