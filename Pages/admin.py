da677fc7717173d6378bf4f05925f742fbf393a3
brentwilson <brent@prmconnect.com>
2024-09-22 19:12:42 -0700

    re-add Page admin_py

--- Pages/admin.py
+++ Pages/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'is_active']
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
+    list_filter = ['is_active', 'created_at']
+    date_hierarchy = 'created_at'
+    fieldsets = [
+        (None, {
+            'fields': ['title', 'slug', 'content']
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ['is_active']
+        }),
+    ]
+    readonly_fields = ['created_at', 'updated_at']
+    list_per_page = 10
+    actions = ['make_active', 'make_inactive']
+
+    def make_active(self, request, queryset):
+        queryset.update(is_active=True)
+    make_active.short_description = 'Mark selected pages as active'
+
+    def make_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+    make_inactive.short_description = 'Mark selected pages as inactive'
--- Pages/views.py
+++ Pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
 
 def index(request):
     context = { 'title': 'Home' }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
+
+def page(request, slug):
+    context = { 'title': slug }
+    page = Page.objects.get(slug=slug)
+    context['page'] = page
+    return render(request, 'page.html', context)
