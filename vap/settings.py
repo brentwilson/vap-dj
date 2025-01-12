6e473c6c2da32ec3239fb70221b15bbc8f881025
brentwilson <brent@freshguava.com>
2025-01-12 12:22:44 -0800

    add coolify url

--- vap/settings.py
+++ vap/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-02f@i#k+74+lqbgh5jw^3+)*(vu+tzqoi)(dl4!ia6)uc*k^y3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'n8gs0gws8kk4sg8okcg0o8kg.85.31.232.250.sslip.io']
 
 
 # Application definition
